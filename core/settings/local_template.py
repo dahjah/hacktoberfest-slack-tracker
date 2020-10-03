932df57c5e7b9df261a380cdcbe2eb76fcc953a9
DJ Gillespie <dj@monumetric.com>
2020-10-02 21:44:52 -0600

    added local_template, and filled in production.py

--- core/settings/local_template.py
+++ core/settings/local_template.py
@@ -1,22 +1,21 @@
 import dj_database_url
-import os
 
 # Local
-DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DEFAULT_CONNECTION = dj_database_url.parse('')
 # Production
 # DEFAULT_CONNECTION = dj_database_url.parse('')
 
 DATABASES = {"default": DEFAULT_CONNECTION, }
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'r#su6vxqe4_3#p(2jkofv=e1gq1^67yv2ay*+whsvodis^fv+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 GITHUB_AUTH = {
-               "username": os.environ.get("GITHUB_USER"),
-               "token": os.environ.get("GITHUB_TOKEN")
+               "username": "",
+               "token": ""
                }
